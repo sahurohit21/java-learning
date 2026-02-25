3523b58aa720640e7947d6478eb7d694e255a299
rohit-sahu-dev <sahu.rohit2101@gmail.com>
2026-02-25 15:53:56 +0530

    update

--- 7. Conditional_Statements/If_else_ladder.java
+++ 7. Conditional_Statements/If_else_ladder.java
@@ -1,13 +1,18 @@
 public class If_else_ladder {
-    public static void main(String[] args) {
-        int a = 10, b = 15, c = 5;
-        if (a > b && a > c) {
-            System.out.println("a is greater : " + a);
-        } else if (b > c) {
-            System.out.println("b is greater : " + b);
-        } else {
-            System.out.println("c is greater : " + c);
-        }
+    public If_else_ladder() {
     }
 
+    public static void main(String[] var0) {
+        byte var1 = 10;
+        byte var2 = 15;
+        byte var3 = 5;
+        if (var1 > var2 && var1 > var3) {
+            System.out.println("a is greater : " + var1);
+        } else if (var2 > var3) {
+            System.out.println("b is greater : " + var2);
+        } else {
+            System.out.println("c is greater : " + var3);
+        }
+
+    }
 }
